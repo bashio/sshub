8039b229d4f4da5045b67ada6175b8f91254a97c
Terry Cai <gidcai@gmail.com>
2019-02-14 16:24:12 +0800

    Update shadowsocks.sh

--- proxy/shadowsocks.sh
+++ proxy/shadowsocks.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 RED_COLOR='\033[0;31m'
 GREEN_COLOR='\033[0;32m'
 NO_COLOR='\033[0m'
@@ -100,9 +98,9 @@ get_unused_port()
 config(){
 
   # config encryption password
-  read -p "Password used for encryption (Default: shellhub):" sspwd
+  read -p "Password used for encryption (Default: w3cub.service):" sspwd
   if [[ -z "${sspwd}" ]]; then
-    sspwd="shellhub"
+    sspwd="w3cub.service"
   fi
   echo -e "encryption password: ${GREEN_COLOR}${sspwd}${NO_COLOR}"
 
@@ -158,7 +156,7 @@ config(){
     fi
   done
   # add shadowsocks config file
-  cat <<EOT > /etc/shadowsocks.json
+  cat <<EOT > /etc/shadowsocks-libev/config.json
 {
   "server":"0.0.0.0",
   "server_port":${server_port},
@@ -187,13 +185,14 @@ containsIgnoreCase(){
 }
 
 addTcpPort(){
-  tcpPort=${1}
+  tcpPort=${server_port}
   cat /etc/*elease | grep -q VERSION_ID=\"7\"
   if [[ $? = 0 ]]; then
     firewall-cmd --zone=public --add-port=${tcpPort}/tcp --permanent
     firewall-cmd --reload
   else
-    iptables -I INPUT -p tcp -m tcp --dport ${tcpPort} -j ACCEPT
+    # iptables -I INPUT -p tcp -m tcp --dport ${tcpPort} -j ACCEPT
+    iptables -4 -A INPUT -p tcp --dport ${tcpPort} -m  -j ACCEPT
     service iptables save
   fi
 }
@@ -231,7 +230,8 @@ install_shadowsocks(){
     yum install net-tools -y
     yum install python-setuptools -y && easy_install pip
   fi
-  pip install shadowsocks
+  # pip install shadowsocks
+  apt install shadowsocks-libev -y
 }
 
 # stop firewall
@@ -245,8 +245,68 @@ stop_firewall(){
   fi
 }
 
-main(){
+optimize_ss() {
+  cat <<EOT > /etc/sysctl.d/local.conf
+# max open files
+fs.file-max = 51200
+# max read buffer
+net.core.rmem_max = 67108864
+# max write buffer
+net.core.wmem_max = 67108864
+# default read buffer
+net.core.rmem_default = 65536
+# default write buffer
+net.core.wmem_default = 65536
+# max processor input queue
+net.core.netdev_max_backlog = 4096
+# max backlog
+net.core.somaxconn = 4096
+# resist SYN flood attacks
+net.ipv4.tcp_syncookies = 1
+# reuse timewait sockets when safe
+net.ipv4.tcp_tw_reuse = 1
+# turn off fast timewait sockets recycling
+net.ipv4.tcp_tw_recycle = 0
+# short FIN timeout
+net.ipv4.tcp_fin_timeout = 30
+# short keepalive time
+net.ipv4.tcp_keepalive_time = 1200
+# outbound port range
+net.ipv4.ip_local_port_range = 10000 65000
+# max SYN backlog
+net.ipv4.tcp_max_syn_backlog = 4096
+# max timewait sockets held by system simultaneously
+net.ipv4.tcp_max_tw_buckets = 5000
+# turn on TCP Fast Open on both client and server side
+net.ipv4.tcp_fastopen = 3
+# TCP receive buffer
+net.ipv4.tcp_rmem = 4096 87380 67108864
+# TCP write buffer
+net.ipv4.tcp_wmem = 4096 65536 67108864
+# turn on path MTU discovery
+net.ipv4.tcp_mtu_probing = 1
+# for high-latency network
+net.ipv4.tcp_congestion_control = hybla
+# for low-latency network, use cubic instead
+net.ipv4.tcp_congestion_control = cubic
+EOT
+sysctl --system
 
+}
+
+
+start_service(){
+  systemctl daemon-reload
+  systemctl enable shadowsocks-libev
+  systemctl start shadowsocks-libev
+}
+
+stop_service(){
+  systemctl stop shadowsocks-libev
+}
+
+
+main(){
   #check root permission
   isRoot=$( isRoot )
   if [[ "${isRoot}" != "true" ]]; then
@@ -256,10 +316,9 @@ main(){
     intro
     config
     install_shadowsocks
-    #addTcpPort ${server_port}
-    stop_firewall
-    # run background
-    `ssserver -c /etc/shadowsocks.json --user nobody -d start` >/dev/null
+    addTcpPort
+    # stop_firewall
+    start_service
     successInfo
   fi
 }
